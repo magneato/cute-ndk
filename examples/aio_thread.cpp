b3f81616a028c834d42e9061603fcefa438b7fba
Shaovie <Shaovie@264c20cc-25e0-11df-8ad4-c9747f8b2119>
2010-05-21 02:45:01 +0000

    no bug

--- examples/aio_thread.cpp
+++ examples/aio_thread.cpp
@@ -23,9 +23,14 @@
 #include <ndk/logger.h>
 #include <ndk/condition.h>
 
+#ifdef METHOD_TRACE 
+#undef METHOD_TRACE
+#define METHOD_TRACE(X) 
+#endif
 static ndk::logger *main_log = ndk::log_manager::instance()->get_logger("root.aio_thread");
 
 std::deque<std::string> file_list;
+std::deque<std::string> const_file_list;
 ndk::thread_mutex file_list_mtx;
 
 int concurrent_aio_requests_keeping = 64;
@@ -59,7 +64,7 @@ enum
   AIO_NOTCANCELED
 };
 
-#define MAX_AIO_QUEUE_LIST_SIZE  4096
+#define MAX_AIO_QUEUE_LIST_SIZE  100
 
 class aio_opt_t;
 class asynch_handler
@@ -83,7 +88,8 @@ public:
     offset_(0),
     buffer_(0),
     aio_handler_(0),
-    next_(0)
+    next_(0),
+    ptr_(0)
   { }
 
   inline ndk::ndk_handle handle() const
@@ -116,6 +122,7 @@ protected:
     this->buffer_   = 0;
     this->aio_handler_ = 0;
     this->next_ = 0;
+    this->ptr_  = 0;
   }
 protected:
   ndk::ndk_handle handle_;
@@ -129,6 +136,7 @@ protected:
   asynch_handler *aio_handler_;
 
   aio_opt_t *next_;
+  aio_opt_t *ptr_;
 };
 class asynch_file_io: public ndk::task_base
 {
@@ -139,6 +147,8 @@ public:
     queue_list_mtx_(),
     not_empty_cond_(queue_list_mtx_),
     running_list_(0),
+    running_list_tail_(0),
+    free_running_list_(0),
     free_list_(0)
   { }
 
@@ -154,6 +164,7 @@ public:
                 int optcode,
                 int priority)
   {
+    METHOD_TRACE(main_log);
     assert(nbytes > 0);
     assert(handle != NDK_INVALID_HANDLE);
     assert(handler != 0);
@@ -162,13 +173,13 @@ public:
     aio_opt_t *aioopt = this->alloc_aio_opt();
     if (aioopt == 0) return AIO_WOULDBLOCK;
     
-    aioopt->aio_handler_ = handler;
-    aioopt->handle_   = handle;
-    aioopt->i_nbytes_ = nbytes;
-    aioopt->buffer_   = buff;
-    aioopt->offset_   = offset;
-    aioopt->opcode_   = optcode;
-    aioopt->priority_ = priority;
+    aioopt->aio_handler_  = handler;
+    aioopt->handle_       = handle;
+    aioopt->i_nbytes_     = nbytes;
+    aioopt->buffer_       = buff;
+    aioopt->offset_       = offset;
+    aioopt->opcode_       = optcode;
+    aioopt->priority_     = priority;
 
     return this->enqueue_aio_request(aioopt);
   }
@@ -180,6 +191,7 @@ public:
    */
   int cancel_aio(ndk::ndk_handle handle)
   {
+    METHOD_TRACE(main_log);
     assert(handle != NDK_INVALID_HANDLE);
     int result = AIO_CANCELED;
     {
@@ -210,37 +222,22 @@ public:
             itor = itor->next_;
           }
         }
-      }
+      } // end of `if (this->queue_list_ != 0)'
     }
-
-    // 
-    ndk::guard<ndk::thread_mutex> g(this->running_list_mtx_);
-    if (this->running_list_ == 0) return result;
-
-    aio_opt_t *itor = this->running_list_;
-    while (itor != 0)
-    {
-      if (itor->handle_ == handle)
-      {
-        result = AIO_NOTCANCELED;
-        break;
-      }
-    }
-
-    return result;
+    return this->find_in_running_list(handle);
   }
 protected:
   virtual int svc()
   {
     while (1)
     {
+      METHOD_TRACE(main_log);
       aio_opt_t *running_list = 0;
       {
         ndk::guard<ndk::thread_mutex> g(this->queue_list_mtx_);
         while (this->queue_list_ == 0)
-        {
           this->not_empty_cond_.wait(0);
-        }
+
         aio_opt_t *tail = this->queue_list_->next_;
         aio_opt_t *prev = this->queue_list_;
         running_list = this->queue_list_;
@@ -258,7 +255,9 @@ protected:
   }
   void handle_aio_requests(aio_opt_t *running_list)
   {
+    METHOD_TRACE(main_log);
     this->enqueue_to_running_list(running_list);
+
     for (aio_opt_t *aioopt = running_list; aioopt != 0; aioopt = aioopt->next_)
     {
       int result = 0;
@@ -290,12 +289,14 @@ protected:
       {
         aioopt->errcode_  = errno;
       }
-      this->dequeue_from_running_list(aioopt, callback);
+      (aioopt->aio_handler_->*callback)(aioopt);
+      this->dequeue_from_running_list(aioopt);
     }
     this->free_aio_opt(running_list);
   }
   int enqueue_aio_request(aio_opt_t *aioopt)
   {
+    METHOD_TRACE(main_log);
     ndk::guard<ndk::thread_mutex> g(this->queue_list_mtx_);
     if (this->queue_list_ == 0)
     {
@@ -319,71 +320,141 @@ protected:
   }
   void enqueue_to_running_list(aio_opt_t *running_list)
   {
+    METHOD_TRACE(main_log);
+    assert(running_list != 0);
     ndk::guard<ndk::thread_mutex> g(this->running_list_mtx_);
-    aio_opt_t *itor = running_list;
+
+    aio_opt_t *aioopt = 0;
     if (this->running_list_ == 0)
-      this->running_list_ = running_list;
-    else
     {
-      this->running_list_->next_;
-      aio_opt_t *tail = this->running_list_;
-      while (tail->next_ != 0) tail = tail->next_;
-      tail->next_ = running_list; 
+      aioopt = this->alloc_aio_opt_i(this->free_running_list_);
+      aioopt->ptr_ = running_list;
+      this->running_list_tail_ = aioopt;
+      this->running_list_ = aioopt;
+      running_list = running_list->next_;
+    }
+
+    aio_opt_t *itor = running_list;
+    for (; itor != 0; itor = itor->next_)
+    {
+      aioopt = this->alloc_aio_opt_i(this->free_running_list_);
+      aioopt->ptr_ = itor;
+      this->running_list_tail_->next_ = aioopt;
+      this->running_list_tail_ = aioopt;
     }
   }
-  void dequeue_from_running_list(aio_opt_t *aioopt, 
-                                 void (asynch_handler::*callback)(const aio_opt_t *))
+  void dequeue_from_running_list(aio_opt_t *aioopt)
   {
+    METHOD_TRACE(main_log);
     ndk::guard<ndk::thread_mutex> g(this->running_list_mtx_);
-    (aioopt->aio_handler_->*callback)(aioopt);
     assert(this->running_list_ != 0);
 
     aio_opt_t *itor = this->running_list_;
+    int result = 0;
+#if 0
     aio_opt_t *prev = itor;
+    int result = 0;
     while (itor != 0)
     {
-      if (itor == aioopt)
+      if (itor->ptr_ == aioopt)
       {
-        if (itor == prev)
+        aio_opt_t *p = itor;
+        if (itor == this->running_list_)
         {
-          itor = itor->next_;
-          this->running_list_ = itor;
-          prev = itor;
+          this->running_list_ = itor->next_;
         }else
         {
           prev->next_ = itor->next_; 
           itor = prev->next_;
         }
+        p->next_ = 0;
+        this->free_aio_opt_i(p, this->free_running_list_);
+        result = 1;
+        break;  // !!
       }else
       {
         prev = itor;
         itor = itor->next_;
       }
     }
+#else
+    aio_opt_t *free_ptr = 0;
+    if (itor->ptr_ == aioopt)
+    {
+      free_ptr = itor;
+      this->running_list_ = itor->next_;
+    }else
+    {
+      while (itor->next_ != 0)
+      {
+        if (itor->next_->ptr_ == aioopt)
+        {
+          if (itor->next_ == this->running_list_tail_)
+            this->running_list_tail_ = itor;
+          free_ptr = itor->next_;
+          itor->next_ = itor->next_->next_;
+          break;
+        }
+        itor = itor->next_;
+      }
+    }
+    assert(free_ptr != 0);
+    free_ptr->next_ = 0;
+    this->free_aio_opt_i(free_ptr, this->free_running_list_);
+#endif
+    if (this->running_list_ == 0 
+        || this->running_list_->next_ == 0)
+      this->running_list_tail_ = this->running_list_;
+  }
+  int find_in_running_list(ndk::ndk_handle handle)
+  {
+    METHOD_TRACE(main_log);
+    ndk::guard<ndk::thread_mutex> g(this->running_list_mtx_);
+    if (this->running_list_ == 0) return 0;
+
+    aio_opt_t *itor = this->running_list_;
+    while (itor != 0)
+    {
+      if (itor->ptr_->handle_ == handle)
+        return AIO_NOTCANCELED;
+    }
+    return 0;
   }
   aio_opt_t *alloc_aio_opt()
   {
+    METHOD_TRACE(main_log);
     ndk::guard<ndk::thread_mutex> g(this->free_list_mtx_);
     if (this->queue_list_size_ > MAX_AIO_QUEUE_LIST_SIZE)
       return 0;
-    if (this->free_list_ == 0)
-      this->free_list_ = new aio_opt_t();
-
-    aio_opt_t *aioopt = this->free_list_;
-    this->free_list_ = this->free_list_->next_;
-    aioopt->reset();
+    aio_opt_t *aioopt = this->alloc_aio_opt_i(this->free_list_);
     ++this->queue_list_size_;
+    return aioopt;
+  }
+  aio_opt_t *alloc_aio_opt_i(aio_opt_t *&aio_list)
+  {
+    if (aio_list == 0)
+      aio_list = new aio_opt_t();
+
+    aio_opt_t *aioopt = aio_list;
+    aio_list = aio_list->next_;
+    aioopt->reset();
     return aioopt;
   }
   void free_aio_opt(aio_opt_t *p)
   {
+    METHOD_TRACE(main_log);
     ndk::guard<ndk::thread_mutex> g(this->free_list_mtx_);
-    aio_opt_t *last = p;
-    int c = 0;
-    for (; last->next_ != 0; last = last->next_) ++c;
-    last->next_ = this->free_list_;
-    this->free_list_ = p;
+    int c = this->free_aio_opt_i(p, this->free_list_);
     this->queue_list_size_ -= c;
+  }
+  int free_aio_opt_i(aio_opt_t *p, aio_opt_t *&aio_list)
+  {
+    aio_opt_t *last = p;
+    int c = 1;
+    for (; last->next_ != 0; last = last->next_) ++c;
+    last->next_ = aio_list;
+    aio_list = p;
+    return c;
   }
 protected:
   size_t queue_list_size_;
@@ -393,12 +464,13 @@ protected:
   ndk::condition<ndk::thread_mutex> not_empty_cond_;
 
   aio_opt_t *running_list_;
+  aio_opt_t *running_list_tail_;
+  aio_opt_t *free_running_list_;
   ndk::thread_mutex running_list_mtx_;
 
   aio_opt_t *free_list_;
   ndk::thread_mutex free_list_mtx_;
 };
-
 
 class aio_test : public asynch_handler
 {
@@ -424,11 +496,12 @@ public:
       delete [] this->buffer_;
       this->buffer_ = 0;
     }
-    if (this->open_ok_  == 0)
-      aio_test::open_next_file();
+    //if (this->open_ok_  == 0)
+     // aio_test::open_next_file();
   }
   int open(const char *file)
   {
+    METHOD_TRACE(main_log);
     this->file_name_ = file;
     this->bytes_to_read_ = block_size*1024;
     this->read_bytes_ = 0;
@@ -449,6 +522,7 @@ public:
   }
   virtual void handle_aio_read(const aio_opt_t *aio_result)
   {
+    METHOD_TRACE(main_log);
     if (aio_result->errcode() == 0)
     {
       if (aio_result->bytes_completed() == 0) // EOF
@@ -478,7 +552,7 @@ public:
         delete this;
       }
       return ;
-    }else
+    }else // end of `if (aio_result->errcode() == 0)'
     {
       main_log->error("read file [%s] offset = %lld errcode = %d",
                       this->file_name_.c_str(),
@@ -498,6 +572,7 @@ public:
   }
   static void open_next_file()
   {
+    METHOD_TRACE(main_log);
     std::string file_name;
     {
       ndk::guard<ndk::thread_mutex> g(file_list_mtx);
@@ -512,6 +587,7 @@ public:
                         (int)(diff.usec()/1000));
       }else
       {
+        file_list = const_file_list;
         main_log->rinfo("no files!");
         return ;
       }
@@ -521,7 +597,7 @@ public:
     aio_test *at = new aio_test();
     if (at->open(file_name.c_str()) != 0)
     {
-      main_log->rinfo("open failed !");
+      main_log->rinfo("aio open failed !");
       delete at;
     }
   }
@@ -553,6 +629,7 @@ void load_dir(const char *dir, std::deque<std::string> &file_list)
       continue;
     file_list.push_back(file_path + "/" + std::string(dp->d_name));
   }
+  const_file_list = file_list;
   closedir(dirp);
 }
 void print_usage()
@@ -610,12 +687,15 @@ int main (int argc, char *argv[])
   aio_task->open(aio_threads);
   ndk::sleep(ndk::time_value(1, 0));
   begin_time.update();
-  for (int i = 0; i < concurrent_aio_requests_keeping; i++)
+  while (1)
   {
-    aio_test::open_next_file();
+    for (int i = 0; i < concurrent_aio_requests_keeping; i++)
+    {
+      aio_test::open_next_file();
+    }
+    ndk::sleep(ndk::time_value(2, 0));
   }
 
-  ndk::sleep(ndk::time_value(10000, 0));
   return 0;
 }
 
