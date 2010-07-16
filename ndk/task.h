4def4743559000c66232a1d258ec4a599cebbb2b
ShaoVie <ShaoVie@264c20cc-25e0-11df-8ad4-c9747f8b2119>
2010-07-16 03:22:38 +0000

    1. 去掉了mem poll 和 thread_manager

--- ndk/task.h
+++ ndk/task.h
@@ -29,9 +29,8 @@ namespace ndk
      * then we'll allocate one dynamically.  Otherwise, we'll use the
      * one passed as a parameter.
      */
-    task(thread_manager *thr_mgr = 0,
-         message_queue *mq = 0)
-      : task_base(thr_mgr),
+    task(message_queue *mq = 0)
+      : task_base(),
       delete_msg_queue_(0),
       msg_queue_(0)
     { 
--- ndk/task_base.h
+++ ndk/task_base.h
@@ -11,10 +11,10 @@
 #define NDK_TASK_BASE_H_
 
 #include "ndk/guard.h"
+#include "ndk/thread.h"
 #include "ndk/unused_arg.h"
 #include "ndk/thread_mutex.h"
 #include "ndk/event_handler.h"
-#include "ndk/thread_manager.h"
 
 namespace ndk
 {
@@ -26,10 +26,8 @@ namespace ndk
   class task_base : public event_handler
   {
   public:
-    task_base(thread_manager *thr_mgr = 0)
-      : thr_count_(0),
-      grp_id_ (-1),
-      thr_mgr_(thr_mgr)
+    task_base()
+      : thr_count_(0)
     { }
 
     virtual ~task_base()
@@ -66,44 +64,25 @@ namespace ndk
                          thread_t thread_ids[] = 0,
                          hthread_t thread_handles[] = 0,
                          int append_thr = 0,
-                         size_t stack_size = 0,
-                         int grp_id = -1)
+                         size_t stack_size = 0)
     {
       guard<thread_mutex> g(this->lock_); // avoid mutithread reentry
       if (this->thr_count_ > 0 && append_thr == 0)
         return 1;
-      else
-      {
-        if (this->thr_count_ > 0 && this->grp_id_ != -1)
-          grp_id = this->grp_id_;
-      }
-      //
-      if (this->thr_mgr_ == 0)
-        this->thr_mgr_ = thread_manager::instance();
 
-      int grp_spawned = -1;
-      grp_spawned = this->thr_mgr_->spawn_n(n_threads,
-                                            &task_base::svc_run,
-                                            (void*)this,
-                                            thread_ids,
-                                            flags,
-                                            thread_handles,
-                                            grp_id,
-                                            stack_size,
-                                            this);
-      if (grp_spawned == -1)
+      //
+
+      if (thread::spawn_n(n_threads,
+                          &task_base::svc_run,
+                          (void*)this,
+                          thread_ids,
+                          flags,
+                          thread_handles,
+                          stack_size) == -1)
         return -1;
       this->thr_count_ += n_threads;
       return 0;
     }
-
-    // Get the thread manager associated with this task.
-    thread_manager *thr_mgr(void) const
-    { return this->thr_mgr_; }
-
-    // 
-    void thr_mgr(thread_manager *thr_mgr)
-    { this->thr_mgr_ = thr_mgr; }
 
     // Returns the number of threads currently running within a task.
     size_t thr_count(void) const
@@ -112,19 +91,13 @@ namespace ndk
       return this->thr_count_; 
     }
 
-    int get_grp_id(void) const
-    {
-      guard<thread_mutex> g(this->lock_);
-      return this->grp_id_;
-    }
-
     // retval 0  Success.
     // retval -1 Failure
-    virtual int wait()
+    virtual int wait(thread_t thread_ids[], size_t size)
     {
-      if (this->thr_mgr() != 0)
-        return this->thr_mgr()->wait_task(this);
-      return -1;
+      for (size_t i = 0; i < size; ++i)
+        thread::join(thread_ids[i]);
+      return 0;
     }
 private:
     // Routine that runs the service routine as a daemon thread.
@@ -132,11 +105,6 @@ private:
     {
       if (args == 0) return 0;
       task_base* t = (task_base *)args;
-      thread_manager *thr_mgr_ptr = t->thr_mgr();
-      if (thr_mgr_ptr)
-      {
-        thr_mgr_ptr->set_thr_state(thread::self(), thread_manager::thr_running);
-      }
       int svc_status = t->svc();
       thread_func_return_t exit_status;
       exit_status = reinterpret_cast<thread_func_return_t> (svc_status);
@@ -159,10 +127,6 @@ private:
 protected:
     // Count of the number of threads running within the task. 
     size_t thr_count_;
-    int    grp_id_;
-
-    // Multi-threading manager.
-    thread_manager *thr_mgr_;
 
     thread_mutex lock_;
   };
