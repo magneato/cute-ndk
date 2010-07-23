16a2bd068b1ee9935c12378f68b76a03cab9207b
Shaovie <Shaovie@264c20cc-25e0-11df-8ad4-c9747f8b2119>
2010-07-23 07:26:40 +0000

    1. 修改了一些对http头的解析方式

--- trunk/apps/fcache/http_client.cpp
+++ trunk/apps/fcache/http_client.cpp
@@ -212,6 +212,7 @@ int http_client::handle_data()
     net_log->rinfo("recv an uncompletd request");
     return 0;
   }
+  // check method
   if (ptr[0] == 'G' && ptr[1] == 'E' && ptr[2] == 'T')
     this->http_method_ = HTTP_GET;
   else if (ptr[0] == 'H' && ptr[1] == 'E' && ptr[2] == 'A' && ptr[3] == 'D')
@@ -221,12 +222,15 @@ int http_client::handle_data()
     net_log->error("unsupport http method [%s]", ptr);
     return -1;
   }
+
+  // 
   char *uri_begin_p = ptr;
   if (this->http_method_ == HTTP_GET)
     uri_begin_p += sizeof("GET");
   else if (this->http_method_ == HTTP_HEAD)
     uri_begin_p += sizeof("HEAD");
 
+  // get uri
   char *uri_end_p = 0;
   uri_begin_p = http_parser::get_uri(uri_begin_p, &uri_end_p);
   if (uri_begin_p == 0)
@@ -235,7 +239,9 @@ int http_client::handle_data()
     this->response_client(400);
     return -1;
   }
+  this->uri_.assign(uri_begin_p, uri_end_p);
 
+  // get host
   char *host_p_end = 0;
   char *host_p = http_parser::get_value(uri_end_p,
                                         "Host",
@@ -255,40 +261,20 @@ int http_client::handle_data()
     host = ostr.str();
   }
 
-  char *range_p = ::strstr(uri_end_p, "Range: bytes=");
-  int64_t start_pos = 0;
+  // get range
+  int64_t begin_pos = 0;
   int64_t end_pos = -1;
   int partial = 0;
-  if (range_p != 0)
-  {
+  int ret = this->get_range(uri_end_p, begin_pos, end_pos);
+  if (ret == 206)
     partial = 1;
-    char *range_p_end = ::strstr(range_p + sizeof("Range: bytes=") - 1, "\r\n");
-    if (range_p_end == 0)
-    {
-      this->session_desc_ = "Not found '\\r\\n' after 'Range'";
-      this->response_client(400);
-      return -1;
-    }
-    char *endptr = 0;
-    char *dig_pos = range_p + sizeof("Range: bytes=") - 1;
-    if (*dig_pos != '-')
-    {
-      start_pos = ::strtoll(dig_pos, &endptr, 10);
-      char *sep = ::strchr(dig_pos + 1, '-');
-      if (sep != 0 && isdigit(*(sep + 1)))  // 123456-123
-      {
-        end_pos = ::strtoll(sep + 1, &endptr, 10);
-        if (end_pos == 0) end_pos = -1;
-      }
-    }else
-    {
-      start_pos = 0;
-      if (isdigit(*(dig_pos + 1)))
-        end_pos = ::strtoll(dig_pos + 1, &endptr, 10);
-    }
+  else if (ret != 200)
+  {
+    this->response_client(ret);
+    return -1;
   }
 
-  this->uri_.assign(uri_begin_p, uri_end_p);
+  // interface.
   if (this->uri_.length() == 1)
   {
     this->show_status();
@@ -301,19 +287,23 @@ int http_client::handle_data()
   fileinfo_ptr fileinfo = file_manager::instance()->find(url);
   if (!fileinfo)  
   {
-    ndk::ndk_handle fd = ::open(filename.c_str(), O_RDONLY);
     struct stat st;
-    if (fd != NDK_INVALID_HANDLE 
-        && ::fstat(fd, &st) != -1 
-        && ((st.st_mode & S_IFMT) == S_IFREG))
+    if (::stat(filename.c_str(), &st) != -1)
     {
-      fileinfo = fileinfo_ptr(new file_info());
-      fileinfo->length(st.st_size);
-      fileinfo->url(url);
-      fileinfo->filename(filename);
-      fileinfo->mtime(st.st_mtime);
-      file_manager::instance()->insert(fileinfo);
-      if (fd != NDK_INVALID_HANDLE) ::close(fd);
+      if ((st.st_mode & S_IFMT) == S_IFREG)
+      {
+        fileinfo = fileinfo_ptr(new file_info());
+        fileinfo->length(st.st_size);
+        fileinfo->url(url);
+        fileinfo->filename(filename);
+        fileinfo->mtime(st.st_mtime);
+        file_manager::instance()->insert(fileinfo);
+      }else
+      {
+        this->session_desc_ = "Not a regular file";
+        this->response_client(403);
+        return -1;
+      }
     }else
     {
       this->session_desc_ = strerror(errno);
@@ -321,7 +311,6 @@ int http_client::handle_data()
         this->response_client(404);
       else
         this->response_client(503);
-      if (fd != NDK_INVALID_HANDLE) ::close(fd);
       return -1;
     }
   }
@@ -338,12 +327,12 @@ int http_client::handle_data()
   if (end_pos == -1)
     end_pos = fileinfo->length() - 1;
 
-  this->content_length_ = end_pos - start_pos + 1;
+  this->content_length_ = end_pos - begin_pos + 1;
 
   if (this->http_method_ == HTTP_HEAD)
   {
     this->session_desc_ = "Get resource infor!";
-    this->response_client(200, start_pos, end_pos);
+    this->response_client(200, begin_pos, end_pos);
     return -1;
   }
 
@@ -353,7 +342,7 @@ int http_client::handle_data()
       && fileinfo->length() >= (int64_t)g_min_mem_cache_size)
   {
     ++g_cache_file_requests;
-    trans_agent = new mem_cache_transfer(start_pos, this->content_length_);
+    trans_agent = new mem_cache_transfer(begin_pos, this->content_length_);
     result = trans_agent->open(fileinfo);
     if (result != 0)
     {
@@ -370,7 +359,7 @@ int http_client::handle_data()
   if (trans_agent == 0)
   {
     ++g_disk_file_reqeusts;
-    trans_agent = new serial_file_transfer(start_pos, this->content_length_);
+    trans_agent = new serial_file_transfer(begin_pos, this->content_length_);
     if (trans_agent->open(fileinfo) != 0)
     {
       delete trans_agent;
@@ -383,9 +372,9 @@ int http_client::handle_data()
     }
   }
   if (partial == 1)
-    result = this->response_client(206, start_pos, end_pos, fileinfo->length());
+    result = this->response_client(206, begin_pos, end_pos, fileinfo->length());
   else
-    result = this->response_client(200, start_pos, end_pos);
+    result = this->response_client(200, begin_pos, end_pos);
   if (result != -1)
   {
     this->session_id_ = http_sessionmgr::instance()->alloc_sessionid();
@@ -410,7 +399,7 @@ int http_client::handle_data()
   return result;
 }
 int http_client::response_client(int status,
-                                 int64_t start_pos/* = 0*/,
+                                 int64_t begin_pos/* = 0*/,
                                  int64_t end_pos/* = 0*/,
                                  int64_t file_size/* = 0*/)
 {
@@ -420,19 +409,28 @@ int http_client::response_client(int status,
   switch(status)
   {
   case 200:
-    os << "HTTP/1.0 200 OK\r\n";
+    os << "HTTP/1.1 200 OK\r\n";
     break;
   case 206:
-    os << "HTTP/1.0 206 Partial Content\r\n";
-    break;
-  case 404:
-    os << "HTTP/1.0 404 Not Found\r\n";
+    os << "HTTP/1.1 206 Partial Content\r\n";
     break;
   case 400:
-    os << "HTTP/1.0 400 Bad Request\r\n";
+    os << "HTTP/1.1 400 Bad Request\r\n";
+    break;
+  case 403:
+    os << "HTTP/1.1 403 Forbidden\r\n";
+    break;
+  case 404:
+    os << "HTTP/1.1 404 Not Found\r\n";
+    break;
+  case 416:
+    os << "HTTP/1.1 416 Requested range not satisfiable\r\n";
+    break;
+  case 501:
+    os << "HTTP/1.1 501 Not Implemented\r\n";
     break;
   case 503:
-    os << "HTTP/1.0 503 Service Unavailable\r\n";
+    os << "HTTP/1.1 503 Service Unavailable\r\n";
     break;
   default:
     assert(0);
@@ -446,11 +444,11 @@ int http_client::response_client(int status,
   {
     os << "Accept-Ranges: bytes\r\n"
       << "Connection: close\r\n"
-      << "Content-Length: " << end_pos - start_pos + 1 << "\r\n";
+      << "Content-Length: " << end_pos - begin_pos + 1 << "\r\n";
     if (status == 206)
     {
       os << "Content-Range: bytes " 
-        << start_pos 
+        << begin_pos 
         << "-" 
         << end_pos 
         << "/" 
@@ -470,6 +468,54 @@ int http_client::response_client(int status,
                    strerror(errno));
   }
   return result < 0 ? -1 : 0;
+}
+int http_client::get_range(const char *str,
+                           int64_t &begin_pos,
+                           int64_t &end_pos)
+{
+  // Range: bytes=0-499
+  char *range_end_p = 0;
+  char *range_p = http_parser::get_value(str,
+                                         "Range",
+                                         sizeof("Range") - 1,
+                                         &range_end_p);
+  if (range_p == 0)
+    return 200;
+
+  char *value_ptr = range_p + ::strspn(range_p, "bytes= \t");
+
+  net_log->error("%s", value_ptr);
+  char *xx = value_ptr;
+  while (*xx && xx < range_end_p)
+  {
+    if (*xx == ',') break;
+    ++xx;
+  }
+  if (xx != range_end_p)
+  {
+    this->session_desc_ = "Not support multi Range.";
+    return 501;
+  }
+
+  char *endptr = 0;
+  if (isdigit(*value_ptr))
+  {
+    begin_pos = ::strtoll(value_ptr, &endptr, 10);
+    char *sep = ::strchr(value_ptr + 1, '-');
+    if (sep != 0 && isdigit(*(sep + 1)))  // 123456-123
+      end_pos = ::strtoll(sep + 1, &endptr, 10);
+  }else
+  {
+    begin_pos = 0;
+    if (isdigit(*(value_ptr + 1)))
+      end_pos = ::strtoll(value_ptr + 1, &endptr, 10);
+    else
+    {
+      this->session_desc_ = "Requested range not satisfiable";
+      return 416;
+    }
+  }
+  return 206;
 }
 int http_client::show_status()
 {
